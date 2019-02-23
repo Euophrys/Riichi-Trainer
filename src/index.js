7021db07efae22e93bb8ad4b4d1649a49e0cfc4d
Amber <erzz197+gh@gmail.com>
2019-02-23 15:46:57 -0400

    Revert "Initial commit."
    This reverts commit 679664b9ba233ab24f9d40de57a3246d4afdd2cc.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-
-import UkeireQuiz from './states/UkeireQuiz';
-
-ReactDOM.render(<UkeireQuiz />, document.getElementById('root'));
