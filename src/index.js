5465ee6734c41abebe736bfe355a5b8aa9d523f7
Amber <erzz197+git@gmail.com>
2019-03-02 12:09:01 -0400

    Conversion functions; ordering
    Souzu is now ordered last.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-import UkeireQuiz from './states/UkeireQuiz';
+//import UkeireQuiz from './states/UkeireQuiz';
 //import ScoreToPointsQuiz from './states/ScoreToPointsQuiz';
 //import HandExplorer from './states/HandExplorer';
+import ReplayAnalysis from './states/ReplayAnalysis';
 
-ReactDOM.render(<UkeireQuiz />, document.getElementById('root'));
+ReactDOM.render(<ReplayAnalysis />, document.getElementById('root'));
