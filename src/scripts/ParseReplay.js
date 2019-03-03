0d0a8747e2dcd4db2f68465080f7174e30dbb79b
Amber <erzz197+git@gmail.com>
2019-03-02 22:20:34 -0400

    Added safety evaluations

--- src/scripts/ParseReplay.js
+++ src/scripts/ParseReplay.js
@@ -1,9 +1,10 @@
 import { allTilesRemaining } from '../Constants';
 import { convertTenhouHandToHand, convertHandToTenhouString } from './HandConversions';
-import { convertTenhouTilesToIndex, getTileAsText } from './TileConversions';
-import { CalculateDiscardUkeire, CalculateUkeireFromOnlyHand } from './UkeireCalculator';
+import { convertTenhouTilesToIndex, getTileAsText, convertTilesToAsciiSymbols } from './TileConversions';
+import { CalculateDiscardUkeire, CalculateUkeireFromOnlyHand, CalculateUkeire } from './UkeireCalculator';
 import CalculateMinimumShanten from './ShantenCalculator';
-import { evaluateBestDiscard } from './Evaluations';
+import { evaluateBestDiscard, evaluateSafestDiscards } from './Evaluations';
+import { freemem } from 'os';
 
 export function parseRounds(replayText) {
     let games = replayText.split("<INIT ");
@@ -62,7 +63,9 @@ export function parseRound(roundText, player) {
     for(let i = 0; i < 4; i++) {
         players.push({
             hand: parseHand(roundText, i),
-            discards: []
+            discards: [],
+            discardsAfterRiichi: [],
+            riichiTile: -1
         });
     }
 
@@ -73,7 +76,7 @@ export function parseRound(roundText, player) {
     let regex = /<(\w{1})(.+?)\/>/g;
     let whoRegex = /who="(\d)"/;
     let messages = [];
-    messages.push("Your starting hand was " + convertHandToTenhouString(players[player].hand));
+    messages.push("Your starting hand is " + convertHandToTenhouString(players[player].hand));
     let match;
 
     do {
@@ -93,7 +96,39 @@ export function parseRound(roundText, player) {
                 }
             }
 
-            if(actionInfo.riichi) continue;
+            if(actionInfo.riichi) {
+                let who = whoRegex.exec(match[2]);
+
+                if(!who) {
+                    messages.push("Ryuukyoku.");
+                    break;
+                }
+
+                if(parseInt(who[1]) === player) {
+                    if(players[player].riichiTile > -1) {
+                        messages.push("You declared riichi. Ending analysis.");
+                        break;
+                    }
+                    continue;
+                }
+
+                if(players[parseInt(who[1])].riichiTile > -1) continue;
+
+                let shanten = CalculateMinimumShanten(players[player].hand);
+                let message = `Player ${who[1]} declared riichi. `;
+
+                if(shanten > 1) {
+                    message += `You are still ${shanten} tiles from ready, so you should fold.`;
+                } else if (shanten === 1) {
+                    message += `You are one tile from ready. You should consider folding.`;
+                }
+
+                messages.push(message);
+                players[parseInt(who[1])].riichiTile = -2;
+                continue;
+            }
+
+
             if(actionInfo.end) {
                 let who = whoRegex.exec(match[2]);
                 messages.push(`Player ${who[1]} won the round.`);
@@ -106,11 +141,29 @@ export function parseRound(roundText, player) {
                 if(actionInfo.player === player) {
                     let message = analyzeDiscard(players[player].hand, index, remainingTiles);
                     messages[messages.length - 1] += message;
+                    message = analyzeSafestDiscard(players[player].hand, index, players, remainingTiles);
+                    messages[messages.length - 1] += message;
                 }
 
                 players[actionInfo.player].hand[index]--;
-                players[actionInfo.player].discards.push(index);
-                remainingTiles[index]--;
+
+                if(actionInfo.player !== player) {
+                    remainingTiles[index]--;
+                }
+                
+                for(let i = 0; i < players.length; i++) {
+                    if(players[i].riichiTile > -1) {
+                        players[i].discardsAfterRiichi.push(index);
+                    }
+                    else if(i === actionInfo.player) {
+                        players[actionInfo.player].discards.push(index);
+                    }
+
+                    if(players[i].riichiTile === -2) {
+                        players[i].riichiTile = index;
+                    }
+                }
+
                 continue;
             }
 
@@ -119,7 +172,8 @@ export function parseRound(roundText, player) {
                 players[actionInfo.player].hand[index]++;
 
                 if(actionInfo.player === player) {
-                    messages.push(`You drew the ${getTileAsText(index)}. `);
+                    messages.push(`You drew the ${getTileAsText(index)}. (${convertHandToTenhouString(players[player].hand)})|`);
+                    remainingTiles[index]--;
                 }
 
                 continue;
@@ -128,6 +182,52 @@ export function parseRound(roundText, player) {
     } while (match);
 
     return messages;
+}
+
+const safetyRatingExplanations = [
+    "you shouldn't see this message, tell me if you do",
+    "non-suji 4/5/6", "non-suji 3/7", "non-suji 2/8", "one-chance", "non-suji 1/9",
+    "first honor tile", "suji 3/7", "suji 2/8", "suji 4/5/6", "second honor tile",
+    "first suji terminal", "second suji terminal", "third suji terminal / third honor",
+    "fourth suji terminal / fourth honor", "genbutsu"
+]
+
+function analyzeSafestDiscard(playerHand, chosenTile, players, remainingTiles) {
+    let riichis = 0;
+    let totalSafety = Array(38).fill(0);
+
+    for(let i = 0; i < players.length; i++) {
+        if(players[i].riichiTile >= 0) {
+            riichis++;
+
+            let safety = evaluateSafestDiscards(
+                playerHand,
+                players[i].discards,
+                remainingTiles,
+                players[i].discardsAfterRiichi,
+                players[i].riichiTile
+            );
+
+            for(let j = 0; j < totalSafety.length; j++) {
+                totalSafety[j] += safety[j];
+            }
+        }
+    }
+
+    if(riichis === 0) return "";
+    
+    let chosenSafety = totalSafety[chosenTile];
+    let result = `The ${getTileAsText(chosenTile, true)}'s safety rating is ${totalSafety[chosenTile]} (${safetyRatingExplanations[Math.floor(chosenSafety / riichis)]}).|`;
+    let bestSafety = Math.max(...totalSafety);
+    let bestChoice = totalSafety.indexOf(bestSafety);
+
+    if(bestSafety === chosenSafety) {
+        result += `That was the safest tile.|`;
+    } else {
+        result += `Safest tile: ${getTileAsText(bestChoice, true)}, with a safety rating of ${bestSafety} (${safetyRatingExplanations[Math.floor(bestSafety / riichis)]}).|`;
+    }
+
+    return result;
 }
 
 function analyzeDiscard(playerHand, chosenTile, remainingTiles) {
@@ -141,25 +241,29 @@ function analyzeDiscard(playerHand, chosenTile, remainingTiles) {
     let shanten = CalculateMinimumShanten(hand);
     let handUkeire = CalculateUkeireFromOnlyHand(hand, allTilesRemaining.slice(), CalculateMinimumShanten).value;
     let bestTile = evaluateBestDiscard(ukeire);
-    let result = `You chose to discard the ${getTileAsText(chosenTile, true)}, which `;
+    let result = `Discard: ${getTileAsText(chosenTile, true)}, `;
 
     if (chosenUkeire > 0 || shanten === 0) {
-        result += `results in ${chosenUkeire} tiles that can improve the hand. `;
+        let ukeireTiles = CalculateUkeire(hand, remainingTiles, CalculateMinimumShanten, shanten);
+        result += `with ${chosenUkeire} tiles that can improve the hand: ${convertTilesToAsciiSymbols(ukeireTiles.tiles)}|`;
     }
     else {
-        result += `lowers your shanten - you are now further from ready. `
+        result += `which lowers your shanten.|`
     }
 
     if (chosenUkeire < bestUkeire) {
-        result += `The most efficient tile to discard, the ${getTileAsText(bestTile, true)}, `;
-        result += `would have resulted in ${bestUkeire} tiles being able to improve your hand. `;
+        hand[chosenTile]++;
+        hand[bestTile]--;
+        let bestUkeireTiles = CalculateUkeire(hand, remainingTiles, CalculateMinimumShanten);
+        result += `Most efficient: the ${getTileAsText(bestTile, true)}, `;
+        result += `with ${bestUkeire} tiles being able to improve your hand: ${convertTilesToAsciiSymbols(bestUkeireTiles.tiles)}|`;
     }
     else {
-        result += "That was the best choice. Good work! ";
+        result += "That was the most efficient choice.|";
     }
 
     if (shanten <= 0 && handUkeire === 0) {
-        result += " You are now in keishiki tenpai. Your hand is ready, but all the winning tiles are in your hand. This doesn't count as ready. ";
+        result += "You are now in keishiki tenpai. Your hand is ready, but all the winning tiles are in your hand. This doesn't count as ready.|";
     }
 
     return result;
