32fb7524dabfefd4598cd49f6de8262ce7f98869
Amber <erzz197+git@gmail.com>
2019-06-24 16:30:29 -0300

    Added gyakuten trainer

--- src/scripts/Utils.js
+++ src/scripts/Utils.js
@@ -1,5 +1,6 @@
+
 export function randomInt(max, min = 0) {
-    return Math.floor(Math.random() * max + min);
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
 export function getRandomItem(array) {
@@ -23,4 +24,20 @@ export function shuffleArray(array) {
     }
   
     return array;
+}
+
+export function validateFu(fu, currentFu) {
+    fu = Math.max(fu, 20);
+
+    if (fu !== 25) {
+        if (fu < currentFu) {
+            fu = Math.floor(fu / 10) * 10;
+        } else {
+            fu = Math.ceil(fu / 10) * 10;
+        }
+    }
+
+    fu = Math.min(fu, 130);
+
+    return fu;
 }
